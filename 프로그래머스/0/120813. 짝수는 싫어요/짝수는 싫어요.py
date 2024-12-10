a4bc12dacffb26ef4b070d4f3e7d77df0eb9648c
highcat <odr3045@gmail.com>
2024-12-10 09:43:42 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.py
@@ -0,0 +1,9 @@
+def solution(n):               # 숫자 n을 입력하면
+    answer = []                # 결과를 저장할 빈 리스트를 생성
+
+    
+              #range함수를 사용해 1부터 n+1이 나오면 멈추고 2씩 건너뛴다
+    for i in range(1,n+1,2):   # 나온 홀수 값을 i 변수에 하나씩 저장한다.
+        answer.append(i)       # 홀수를 하나씩 리스트 answer
+
+    return answer              # answer 리스트를 함수 결과로 돌려줌
