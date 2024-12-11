76503bf4d976432b7b987c148abf273fe9d5ab63
highcat <odr3045@gmail.com>
2024-12-11 09:21:02 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.py
@@ -0,0 +1,3 @@
+def solution(my_string):     # 입출력 예시의 "jaron"을 쓸 필요 없이 코드를 짧게 축약
+    answer = my_string[::-1] # 리스트로 시작 점과 끝 점을 모두 포함, 역순으로 출력 '-1' 
+    return answer            # 문자열로 지정된 'my_string' 결과값을 리턴합니다.
