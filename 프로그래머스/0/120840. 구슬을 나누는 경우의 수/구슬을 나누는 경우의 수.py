7cc121e2e509f370b49dd112aca789dd10e1e38d
value_ga <odr3045@gmail.com>
2025-02-07 10:00:03 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.01 ms, Memory: 9.95 MB -BaekjoonHub

--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
@@ -7,21 +7,21 @@ def solution(balls, share):
 #    (n-m)! x m!
 
     
-    #분모
-    moo = 1
+    #분자
+    ja = 1
 #     7! 
 #   = 7x6x5x4x3x2x1      
     for i in range(n,m,-1):     # '7','6'
-        moo *= i # 7x6
-#            └> moo = moo * i
+        ja *= i # 7x6
+#            └> ja = ja * i
 
-    #분자
-    ja = 1
+    #분모
+    moo = 1
 #      (7 - 5)! - 5! 
 #    =  2 x 1   - 5x4x3x2x1
     for i in range(n-m,0,-1):   # '2','1'
-        ja *= i # 2x1
-#           └> ja = ja * i
+        moo *= i # 2x1
+#           └> moo = moo * i
 
 
 # 분모와 분자에 공통되는 연산 값을 소거함
@@ -29,6 +29,6 @@ def solution(balls, share):
 #    ────────────────────────   = 42 ÷ 2
 #       2x1 (소거 5x4x3x2x1)
 
-    return moo // ja #          = 21
+    return ja // moo #          = 21
 
 print(solution(7,5))
